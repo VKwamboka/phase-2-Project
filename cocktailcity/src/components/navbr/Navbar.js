c241804d2c1b193f65eedd1801976e2c1d42ccea
verah <verahnyagaka101@gmail.com>
2022-07-25 17:44:11 +0300

    Created Navbar

--- cocktailcity/src/components/navbr/Navbar.js
+++ cocktailcity/src/components/navbr/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-   <nav className=''>
+   <nav className='navbar navbar-expand-lg bg-light'>
+    <Router>
+        <ul className='navbar-nav'>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/'>Home</Link>
+            </li>
+            <li className='navbar-nav'>
+                <Link className='nav-link' to='/search'>Search</Link>
+            </li>
 
+        </ul>
+    </Router>
    </nav>
     
   )
