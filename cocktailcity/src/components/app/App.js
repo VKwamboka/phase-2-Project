fe9c44d8aaf848c1687688e5b92c77d4454654ee
verah <verahnyagaka101@gmail.com>
2022-07-26 17:24:09 +0300

    added more details

--- cocktailcity/src/components/app/App.js
+++ cocktailcity/src/components/app/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Navbar from "../navbr/Navbar";
+import Home from "../home/Home";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Search from "../search/Search";
+import DrinkDetails from "../details/DrinkDetails";
 
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/search' element={<Search/>}/>
+        <Route exact path ='/drink/:drinkId' element={<DrinkDetails/>}/>
+      </Routes>
+    </Router>
   );
 }
 
