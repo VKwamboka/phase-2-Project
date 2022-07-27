b326e6340bbdc30c6884ac82fb989ae0a5f5b10b
verah <verahnyagaka101@gmail.com>
2022-07-27 12:32:53 +0300

    updated view

--- cocktailcity/src/components/cockcard/CockCard.js
+++ cocktailcity/src/components/cockcard/CockCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function CockCard({drinkName, drinkThumb, drinkId}) {
+export default function CockCard({cocktails,drinkName, drinkThumb, drinkId}) {
   return (
     <div>
         <div className="card" style={{width: 18 + 'rem'}}>
@@ -9,7 +9,9 @@ export default function CockCard({drinkName, drinkThumb, drinkId}) {
             <div className="card-body">
                 <h5 className="card-title">{drinkName}</h5>
                 {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                <Link to={"/drink/"+drinkId}  className="btn btn-primary">DETAILS</Link>
+                <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
+                <button type="button" className="btn btn-success btn-sm">♥ Favorite</button>
+                {/* <button type="button" class="btn btn-danger">Delete</button> */}
             </div>
         </div>    
     </div>
