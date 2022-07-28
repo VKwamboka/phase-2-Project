e92a5c326b71f7615d331e2549899bbba8264d8b
verah <verahnyagaka101@gmail.com>
2022-07-28 09:55:07 +0300

    updated the cmponent

--- cocktailcity/src/components/cockcard/CockCard.js
+++ cocktailcity/src/components/cockcard/CockCard.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function CockCard({cocktails,drinkName, drinkThumb, drinkId}) {
+export default function CockCard({ drinkName, drinkThumb, drinkId}) {
+  
+
+  // const {drinkName, drinkThumb, drinkId} = drinks
+
+  // const [cocktails, setCocktails] = useState([]);
+  //   const [favoriteVisible, setFavoriteVisible] = useState(true);
+  //   const cocktailsToDisplay = cocktails.filter((cocktail) => favoriteVisible || cocktail.isFavorite);
+
+
   return (
     <div>
         <div className="card" style={{width: 18 + 'rem'}}>
@@ -10,7 +19,10 @@ export default function CockCard({cocktails,drinkName, drinkThumb, drinkId}) {
                 <h5 className="card-title">{drinkName}</h5>
                 {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
                 <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
-                <button type="button" className="btn btn-success btn-sm">♥ Favorite</button>
+                <button className='btn btn-success btn-sm'>
+                {/* {drink.isFavorite ? "Unfavorite" : "♥ Favorite" } */}
+                ♥ Favorite
+                </button>
                 {/* <button type="button" class="btn btn-danger">Delete</button> */}
             </div>
         </div>    
