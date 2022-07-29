d1ce827265104ca0b3c47cbd08aec23b2611ff37
verah <verahnyagaka101@gmail.com>
2022-07-29 22:46:03 +0300

    updated component

--- cocktailcity/src/components/cockcard/CockCard.js
+++ cocktailcity/src/components/cockcard/CockCard.js
@@ -12,20 +12,23 @@ export default function CockCard({ drinkName, drinkThumb, drinkId}) {
 
 
   return (
-    <div>
+    <div className='scard'>
         <div className="card" style={{width: 12.6 + 'rem'}}>
-            <img src={drinkThumb} className="card-img-top" alt={drinkName}/>
+            <img src={drinkThumb} className="card-img-top" style={{height: 200 + 'px', width:200 + 'px',}} alt={drinkName}/>
             <div className="card-body">
-                <h5 className="card-title">{drinkName}</h5>
+                {/* <p className="card-title">{drinkName}</p> */}
+                
+                {/* PROBLEM HERE */}
                 {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
                 <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
                 <button className='btn btn-success btn-sm'>
                 {/* {drink.isFavorite ? "Unfavorite" : "♥ Favorite" } */}
-                ♥ Favorite
+               Favorite♥ 
                 </button>
                 {/* <button type="button" class="btn btn-danger">Delete</button> */}
             </div>
         </div>    
+        
     </div>
   )
 }
