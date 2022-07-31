499a3f00c67d15a41edefd8c7d58706cf3912c03
verah <verahnyagaka101@gmail.com>
2022-08-01 00:55:41 +0300

    updated component

--- cocktailcity/src/components/addrecipe/ReciContainer.js
+++ cocktailcity/src/components/addrecipe/ReciContainer.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function ReciContainer({ title, drinkThumb, drinkId}) {
+const cocktailAPI = "http://localhost:8004/drinks";
+
+export default function ReciContainer({ title, drinkThumb, drinkId,handleDeleteClick}) {
   
-
-  // const {drinkName, drinkThumb, drinkId} = drinks
-
-  // const [cocktails, setCocktails] = useState([]);
-  //   const [favoriteVisible, setFavoriteVisible] = useState(true);
-  //   const cocktailsToDisplay = cocktails.filter((cocktail) => favoriteVisible || cocktail.isFavorite);
+  function onDeleteClick(e,drink) {
+    e.preventDefault();
+    fetch(`${cocktailAPI}/${drink.id}`, {
+      method: "DELETE",
+    });
+    handleDeleteClick(drink);
+  }
 
 
   return (
-    <div className='scard'>
-    <div className='app'>
-    <div className='container'>
-    <div className="card" style={{width: 12.6 + 'rem'}}>
-            <img src={drinkThumb} className="card-img-top" style={{height: 200 + 'px', width:200 + 'px',}} alt={title}/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
-                {/* PROBLEM IS HERE */}
-                <button  className='btn btn-success btn-sm'>
-                ♥ Favorite
-                </button>
-                {/* <button type="button" class="btn btn-danger">Delete</button> */}
-            </div>
+  
+    <div className='scards'>
+      <div className='poems-container'>
+        
+        <div className="card" style={{width: 12.8 + 'rem'}}>
+          <img src={drinkThumb} className="card-img-top" style={{height: 200 + 'px', width:200 + 'px',}} alt={title}/>
+          <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+              <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
+              {/* PROBLEM IS HERE */}
+              <button  className='btn btn-success btn-sm'>
+              Fav♥
+              </button>
+              <button type="button" className="btn btn-danger btn-sm" onClick={onDeleteClick}>❌</button>
+          </div>
         </div>  
-    </div>
-    </div>
+      </div>
     </div>
   )
 }
