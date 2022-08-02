01d64fdb0e35dafb6ab7760ec39bbfe39ae0b478
verah <verahnyagaka101@gmail.com>
2022-08-02 22:26:21 +0300

    modified some components

--- cocktailcity/src/components/cockcard/CockCard.js
+++ cocktailcity/src/components/cockcard/CockCard.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
 export default function CockCard({drink, drinkName, drinkThumb, drinkId, addToFavorites}) {
+  const [isFavorite, setFavorites] =useState('♥ Favorite')
+
+  function handleText() {
+    const newText = "♥ Favorite";
+    setFavorites(newText); 
+    if(isFavorite === newText){
+      setFavorites("Unfavorite")
+    }
+  }
 
   return (
     <div className='scard'>
@@ -9,18 +18,13 @@ export default function CockCard({drink, drinkName, drinkThumb, drinkId, addToFa
             <img src={drinkThumb} className="card-img-top" style={{height: 200 + 'px', width:200 + 'px',}} alt={drinkName}/>
             <div className="card-body">
                 <p className="card-title">{drinkName}</p>
-                
-                {/* PROBLEM HERE */}
-                {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          
                 <Link to={"/drink/"+drinkId}  className="btn btn-primary btn-sm">Details</Link>
-                {/* <button className='btn btn-success btn-sm'> */}
-                {/* {drink.isFavorite ? "Unfavorite" : "♥ Favorite" } */}
-               {/* Favorite♥ 
-                </button> */}
-                <button className='btn btn-success btn-sm' onClick={() => addToFavorites(drink)}>
-                  {drinkId.isFavorite ? "Unfavorite" : "♥ Favorite" }
+              
+                <button className='btn btn-success btn-sm' onClick={() =>  handleText()}>
+               {isFavorite}
+                  {/* {drink.isFavorite === true ? "Unfavorite" : "♥ Favorite" } */}
                 </button>
-                {/* <button type="button" class="btn btn-danger">Delete</button> */}
             </div>
         </div>    
         
